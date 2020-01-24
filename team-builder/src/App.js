8afe4ac51a2b9a43f22af3d18e63de2ad1d40ce2
Joe <leftnovbrown@gmail.com>
2020-01-23 20:15:24 -0500

    added components Form and Card. Formatting team list. Browser shows Build Team for now.

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,24 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import MemberCard from './Components/Card';
+import Form from './Components/Form';
 
 function App() {
+  const [member, setMember] = useState([])
+
+  const addNewMember = person => {
+    const newMember = {
+      id: Date.now(),
+      name: person.name,
+      email: person.email,
+      role: person.role
+
+    };
+    setMember([...member, newMember]);
+  }
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Main-container">
+      <h1 className="App-header">Build Team</h1>
+      <div className="form">
+        <Form addNewMember={addNewMember} />
+        <MemberCard member={member} />
+      </div>
+
     </div>
   );
 }
